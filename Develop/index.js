e282732623cd9f69592705965261584048c32b78
Marina Kaeufer <marishutka@yahoo.com>
2022-04-28 20:52:17 -0400

    added array of questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,44 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+{
+    type: "input", 
+    name: "title",
+    message: "What is your project title."
+
+},
+
+{
+    type: "input",
+    name: "sections",
+    message: "Which sections does your project include."
+
+
+},
+
+{ 
+    type: "input",
+    name: "description",
+    message: "What is your project's description."
+
+},
+
+{
+    name: "input",
+    name: "license",
+    message: "Which licence did you use."
+
+}, 
+
+{
+    name: "input", 
+    message: "GitHub", 
+    message: "What is your GitHub username."
+
+
+}
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
