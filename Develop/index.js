05495c30a817d6c23338f0ae62d6e77dd4a2a66e
Marina Kaeufer <marishutka@yahoo.com>
2022-04-30 14:52:42 -0400

    Fix questions array

--- Develop/index.js
+++ Develop/index.js
@@ -1,51 +1,69 @@
 // TODO: Include packages needed for this application
 var inquirer = require('inquirer');
-
+// TODO Don't need this?
+// const data = {
+//     title: "", 
+//     description: "", 
+//     table_contents: [], // Not based on user input
+//     installation: "",
+//     usage: "", 
+//     license: "", // Partially from user
+//     contribution: "",
+//     test: "",
+//     questions: "" // Partiall from user
+// }
+    
 // TODO: Create an array of questions for user input
 const questions = [
 {
     type: "input", 
     name: "title",
     message: "What is your project title?"
-
 },
-
-{
-    type: "input",
-    name: "sections",
-    message: "Which sections do your project include?"
-
-
-},
-
 { 
     type: "input",
     name: "description",
     message: "What is your project's description?"
 
 },
+{ 
+    type: "input",
+    name: "installation",
+    message: "What are your project's installation notes?"
 
+},
+{ 
+    type: "input",
+    name: "usage",
+    message: "What are your usage notes?"
+
+},
+{ 
+    type: "input",
+    name: "contribution",
+    message: "What are your contibution notes?"
+
+},
+{ 
+    type: "input",
+    name: "test",
+    message: "What are your testing instructions?"
+},
 {
     type: "option",
     name: "license",
     message: "Which licence did you use?"
-
+    // Show options
 }, 
-
 {
     type: "input", 
     name: "GitHub", 
     message: "What is your GitHub username?"
-
-
 }, 
-
 {
     type: "input", 
     name: "email", 
     message: "What is your email?"
-
-
 }
 ];
 
@@ -63,9 +81,8 @@ function init() {
     // Display question
     inquirer
         .prompt(questions)
-        .then((answers) => {
-            // Use user feedback for... whatever!!
-            console.log(`Your answers were ${answers}`);
+        .then((responses) => {
+           
         })
         .catch((error) => {
             console.log("Something went wrong " + error);
