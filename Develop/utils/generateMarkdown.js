12ccb666408d667dc845f7d428d19a5df46d7b39
Marina Kaeufer <marishutka@yahoo.com>
2022-04-30 21:31:51 -0400

    Working version, table of content links not working

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -5,7 +5,18 @@ function renderLicenseBadge(license) {
   const permissive = "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"; 
   const lgpl = "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)";
   const copyleft = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-  
+
+  if(license === "Public domain"){
+    return public_domain;
+  }else if(license === "Permissive"){
+    return permissive;
+  }else if(license === "LGPL"){
+    return lgpl;
+  }else if(license === "Copyleft"){
+    return copyleft;
+  }else {
+    return "";
+  }
 }
 
 // TODO: Create a function that returns the license link
@@ -15,7 +26,20 @@ function renderLicenseLink(license) {
   const permissive = "https://en.wikipedia.org/wiki/Permissive_software_license#:~:text=A%20permissive%20software%20license%2C%20sometimes,usually%20including%20a%20warranty%20disclaimer.";
   const lgpl = "https://en.wikipedia.org/wiki/GNU_Lesser_General_Public_License";
   const copyleft = "https://en.wikipedia.org/wiki/Copyleft";
+
+  if(license === "Public domain"){
+    return public_domain;
+  }else if(license === "Permissive"){
+    return permissive;
+  }else if(license === "LGPL"){
+    return lgpl;
+  }else if(license === "Copyleft"){
+    return copyleft;
+  }else {
+    return "";
+  }
 }
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -25,6 +49,17 @@ function renderLicenseSection(license) {
   const lgpl =  "The GNU Lesser General Public License allows you to link to open source libraries in your software. If you simply compile or link an LGPL-licensed library with your own code, you can release your application under any license you want, even a proprietary license. But if you modify the library or copy parts of it into your code, you’ll have to release your application under similar terms as the LGPL.";
   const copyleft =  "Copyleft licenses are also known as reciprocal licenses or restrictive licenses. The most well-known example of a copyleft or reciprocal license is the GPL. These licenses allow you to modify the licensed code and distribute new works based on it, as long as you distribute any new works or adaptations under the same software license. For example, a component’s license might say the work is free to use and distribute for personal use only. So any derivative you create would also be limited to personal use only. (A derivative is any new software you develop that contains the component.)";
 
+  if(license === "Public domain"){
+    return public_domain;
+  }else if(license === "Permissive"){
+    return permissive;
+  }else if(license === "LGPL"){
+    return lgpl;
+  }else if(license === "Copyleft"){
+    return copyleft;
+  }else {
+    return "";
+  }
 }
 
 // TODO: Create a function to generate markdown for README
@@ -36,12 +71,12 @@ function generateMarkdown(data) {
   ${data.description}
 
   ## Table of Contents
-  * [Installation](#Installation)
-  * [Usage](#Usage)
-  * [License](#License)
-  * [Contribution](#Contribution)
-  * [Tests](#Tests)
-  * [Questions](#Questions)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contribution](#contribution)
+  * [Tests](#tests)
+  * [Questions](#questions)
 
   ## Installation
   ${data.installation}
